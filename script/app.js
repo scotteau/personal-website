d3374f41fb299ff6c430bc086feb30e31c74a136
Scott <scotteau@gmail.com>
2020-04-01 15:01:10 +0800

    added nabvar animation & removed scrollmagic trigger debug markers

--- script/app.js
+++ script/app.js
@@ -15,7 +15,7 @@ window.addEventListener("load", () => {
     duration: 500,
     triggerHook: 0.35,
   })
-    .addIndicators({ name: "circle-animation" })
+    // .addIndicators({ name: "circle-animation" })
     .setTween(tween)
     .addTo(controller);
   //endregion
@@ -29,7 +29,7 @@ window.addEventListener("load", () => {
     triggerHook: 0.35,
   })
     .setPin("#item-1", { pushFollowers: false })
-    .addIndicators({ name: "pin animation" })
+    // .addIndicators({ name: "pin animation" })
     .addTo(controller);
 
   const scene3 = new ScrollMagic.Scene({
@@ -38,7 +38,7 @@ window.addEventListener("load", () => {
     triggerHook: 0.35,
   })
     .setPin("#item-2", { pushFollowers: false })
-    .addIndicators()
+    // .addIndicators()
     .addTo(controller);
 
   const scene4 = new ScrollMagic.Scene({
@@ -47,35 +47,60 @@ window.addEventListener("load", () => {
     triggerHook: 0.35,
   })
     .setPin("#item-3", { pushFollowers: false })
-    .addIndicators()
+    // .addIndicators()
     .addTo(controller);
 
   //    endregion
 
   //region Highlighter
-  const highlighter1 = gsap.to(".about .highlighter", 2, {scaleX: 1});
-  const highlighter2 = gsap.to(".work .highlighter", 2, {scaleX: 1});
-  const highlighter3 = gsap.to(".contact .highlighter", 2, {scaleX: 1});
+  const highlighter1 = gsap.to(".about .highlighter", 2, { scaleX: 1 });
+  const highlighter2 = gsap.to(".work .highlighter", 2, { scaleX: 1 });
+  const highlighter3 = gsap.to(".contact .highlighter", 2, { scaleX: 1 });
 
   const aboutMe = new ScrollMagic.Scene({
-    triggerElement:".about h2",
+    triggerElement: ".about h2",
     duration: 400,
-    triggerHook: 0.8
-  }).addIndicators({name: "highlighter"}).setTween(highlighter1).addTo(controller);
+    triggerHook: 0.8,
+  })
+    // .addIndicators({ name: "highlighter" })
+    .setTween(highlighter1)
+    .addTo(controller);
 
   const work = new ScrollMagic.Scene({
-    triggerElement:".work h2",
+    triggerElement: ".work h2",
     duration: 400,
-    triggerHook: 0.8
-  }).addIndicators({name: "highlighter"}).setTween(highlighter2).addTo(controller);
+    triggerHook: 0.8,
+  })
+    // .addIndicators({ name: "highlighter" })
+    .setTween(highlighter2)
+    .addTo(controller);
 
   const contact = new ScrollMagic.Scene({
-    triggerElement:".contact h2",
+    triggerElement: ".contact h2",
     duration: 400,
-    triggerHook: 0.8
-  }).addIndicators({name: "highlighter"}).setTween(highlighter3).addTo(controller);
-//endregion
-});
+    triggerHook: 0.8,
+  })
+    // .addIndicators({ name: "highlighter" })
+    .setTween(highlighter3)
+    .addTo(controller);
+  //endregion
+
+  //region Sticky Navigation bar
+
+  const changeColor = gsap.to("header", 1, { backgroundColor: "#333335" });
+
+  const scene = new ScrollMagic.Scene({
+    triggerElement: ".hero h3",
+    duration: 100,
+    triggerHook: 0.3,
+  })
+    // .addIndicators()
+    .addTo(controller)
+    .setTween(changeColor);
+
+//  endregion
+
+}); // end of method
 
 
 
